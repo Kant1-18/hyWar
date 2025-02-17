92fada4e0097a973aecc3b68276d71fad035f4f1
Quentin Stempfer <quentin.stempfer@supdevinci-edu.fr>
2025-02-17 13:57:26 +0100

    maj prefab suite modef canva

---
+++ Assets/Scripts/ClickCase.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ClickCase : MonoBehaviour
+{
+    public string caseName;
+    // Cette méthode est appelée automatiquement quand l'objet est cliqué
+    private void OnMouseDown()
+    {
+        Debug.Log(gameObject.name + caseName + " a été cliqué !");
+        // Ajoute ici le comportement souhaité lors du clic.
+    }
+}
